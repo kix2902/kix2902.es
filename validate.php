6deab0071acc2003e7e5c2c65addca4914449c13
kix2902 <ismael.kix2902@gmail.com>
2015-08-03 00:57:42 +0200

    Solved bug checking valid data in forms

--- validate.php
+++ validate.php
@@ -6,6 +6,10 @@ function returnHome($message) {
   header('location: ./');
 }
 
+function IsNullOrEmptyString($question){
+    return (!isset($question) || trim($question)==='');
+}
+
 if(isset($_POST['g-recaptcha-response'])) {
   require_once './recaptcha/autoload.php';
   $recaptcha = new \ReCaptcha\ReCaptcha('6Lcj4wkTAAAAAM1DLFAzfyBxE7vT_8PVPnZ-88dQ');
@@ -13,8 +17,11 @@ if(isset($_POST['g-recaptcha-response'])) {
   $resp = $recaptcha->verify($_POST['g-recaptcha-response'], $_SERVER['REMOTE_ADDR']);
   
   if ($resp->isSuccess()) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
     
-    if ((isset($_POST['name'])) and (isset($_POST['email'])) and (isset($_POST['message']))) {
+    if ((!IsNullOrEmptyString($name)) and (!IsNullOrEmptyString($email)) and (!IsNullOrEmptyString($message))) {
         mail("ismael.kix2902@gmail.com","Web contact","Name: ".$_POST['name']."\r\nE-mail: ".$_POST['email']."\r\nMessage: ".$_POST['message']);
         
         returnHome("Message sent successfully.");
